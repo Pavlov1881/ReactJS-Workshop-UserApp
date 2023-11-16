286bd64ea702e37133329d81b9a44fa9513b4414
Anatoli Pavlov <18app81@gmail.com>
2023-11-16 14:50:05 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,9 @@ function App() {
             <Header></Header>
 
             <main className='main'>
+
                 <UserList></UserList>
+
             </main>
 
             <Footer></Footer>
